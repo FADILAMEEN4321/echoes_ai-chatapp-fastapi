1543286a141d571b1c7c3798ac1efdc089d02275
FADIL AMEEN V.M <129923924+FADILAMEEN4321@users.noreply.github.com>
2024-01-11 19:48:16 +0530

    Committing changes to api.py

--- api.py
+++ api.py
@@ -43,11 +43,3 @@ async def fetch_messages():
 def hello():
     return {'hello'}
 
-
-
-# if __name__ == "__main__":
-#     import uvicorn
-
-#     uvicorn.run(app, host="127.0.0.1", port=8000)
-
-
