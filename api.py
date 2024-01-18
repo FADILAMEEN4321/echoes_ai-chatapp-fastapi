e4ecb3c2eabaf755ec486b076656ac006f354ae7
FADIL AMEEN V.M <129923924+FADILAMEEN4321@users.noreply.github.com>
2024-01-18 13:44:56 +0530

    fix: formatted the code and added documentation

--- api.py
+++ api.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, WebSocket
 import os
-import json
 from dotenv import load_dotenv
 import google.generativeai as genai
 import PIL.Image
@@ -9,34 +8,43 @@ import base64
 
 
 load_dotenv()
+
+# Configures the Google API client with the API key loaded from the .env file.
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# Initializes the Gemini GenerativeModel instances for the chatbot and image vision.
 chat_model = genai.GenerativeModel("gemini-pro")
 vision_model = genai.GenerativeModel("gemini-pro-vision")
 
+# Starts a new chat session with the chatbot model.
 chat = chat_model.start_chat(history=[])
 
-
+# Initializes the FastAPI app.
 app = FastAPI()
 
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
+    """
+    websocket_endpoint handles websocket connections.
+
+    It accepts websocket connections, and then enters a loop to handle
+    messages received over the websocket.
+
+    If a text and image is received, it generates a response using the
+    vision model. Otherwise it just generates a text response using the
+    chat model.
+
+    It streams back the response in chunks over the websocket."""
+
     await websocket.accept()
 
     while True:
         message = await websocket.receive_json()
-        print(message)
-
-        print(type(message), "---------------message type=-----")
-
-        checking = message.get("text")
-        print(type(checking), "----------------->text-type<---------")
 
         if message.get("text") and message.get("image"):
-            print("yes------->image hai")
             text = message.get("text")
             image = message.get("image")
-            # image_bytes = image.encode('utf-8')
             image_bytes = base64.b64decode(image)
 
             if message.get("text") == "<FIN>":
@@ -54,16 +62,11 @@ async def websocket_endpoint(websocket: WebSocket):
                 await websocket.close()
                 break
 
-            print("-------------no image")
-            # print('----->chat-list--->',chat.history)
-
             response = chat.send_message([message.get("text")], stream=True)
-
-            # print('----->chat-list after each response--->',chat.history)
 
         for chunk in response:
             await websocket.send_text(chunk.text)
-        # print('----->chat-list complete--->',chat.history)    
+
         await websocket.send_text("<FIN>")
 
 
@@ -76,5 +79,5 @@ async def fetch_messages():
 
 
 @app.get("/")
-def hello():
-    return {"hello"}
+def test():
+    return {"detail": "Yes api is working."}
